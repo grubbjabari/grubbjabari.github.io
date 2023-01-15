af8b7e526d0132f467cc81ee0f73bdc11249554c
Jabari G <grubbjabari@gmail.com>
2023-01-14 21:21:32 -0500

    Testing Emails
    Linked EmailJS

--- js/main.js
+++ js/main.js
@@ -45,3 +45,42 @@ let swiperProjects = new Swiper(".projects_container", {
     mousewheel: true,
     keyboard: true,
   });
+
+/* email */
+
+const contactForm = document.getElementsById('contact-form'),
+    contactName = document.getElementsById('contact-name'),
+    contactEmail = document.getElementsById('contact-email'),
+    contactProject = document.getElementsById('contact-project'),
+    contactMessage = document.getElementsById('contact-message');
+
+const sendEmail = (e) =>{
+    e00.preventDefault()
+
+    //Checks if field has a value
+    if(contactName.value === '' || contactEmail.value === '' || contactProject.value === ''){
+        //Add and remove color
+        contactMessage.classList.remove('color-blue')
+        contactMessage.classList.add('color-red');
+
+        //Show message
+        contactMessage.textContent = 'Write all the input fields!';
+    } else {
+        // ServiceID - templateID - #form - publicKey
+        emailjs.sendForm('service_ji2vq9q', 'template_b4qko2s', '#contact-form','CQmLzhhvxyhzMoVBH')
+            .then(() => {
+                // Show message and add color
+                contactMessage.classList.add('color-blue')
+                contactMessage.textContent = 'Message sent'
+
+                //Remove message after five seconds
+                setTimeout(() => {
+                    contactMessage.textContent = ''
+                 }, 5000)
+            }, (error) =>{
+                alert('Sorry, Something went wrong', error)
+            })
+    }
+}
+
+contactForm.addEventListener('submit', sendEmail)
